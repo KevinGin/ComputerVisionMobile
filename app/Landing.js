d681433c0477dc9db722ecf3a5733ed8d8e48d6f
Kevin Gin <kevinagin@gmail.com>
2017-03-02 12:07:57 -0800

    (feat) has basic Landing page

---
+++ app/Landing.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+	View, 
+	Text,
+	Image,
+  TouchableOpacity,
+	StyleSheet,
+  Dimensions
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+// import PageTwo from './PageTwo';
+
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      	<View style={styles.space}></View>
+      	<Image source={{uri: 'http://res.cloudinary.com/dn4vqx2gu/image/upload/v1487892182/p6ybu5bjev1nnfkpebcc.jpg'}}
+               style={styles.photo} />
+        <View style={styles.buttonContainer}>
+        	<TouchableOpacity
+        	  style={styles.button}>
+            <Text style={styles.buttonText}> Login</Text>
+          </TouchableOpacity>
+        	<TouchableOpacity
+            style={styles.button}>
+            <Text style={styles.buttonText}> Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  space: {
+  	flex: 1,
+  },
+  photo: {
+  	flex: 9,
+  },
+  buttonContainer: {
+  	flex: 1,
+    flexDirection:'row',
+  },
+
+  button: {
+    flex: 1,
+    color: 'red',
+  },
+
+  buttonText: {
+    flex: 1,
+    color: 'black',
+    backgroundColor: 'lightgray'
+  },
+});
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 
-import PageOne from './PageOne';
+import Landing from './Landing';
 import PageTwo from './PageTwo';
 import CameraView from './camera';
 
@@ -11,7 +11,7 @@ export default class App extends Component {
       <Router>
         <Scene key="root">
           <Scene key="CameraView" component={CameraView} title="Camera"  />
-          <Scene key="PageOne" component={PageOne} title="PageOne" initial={true}/>
+          <Scene key="Landing" component={Landing} title="Teacher's Pet: Student" initial={true}/>
           <Scene key="PageTwo" component={PageTwo} title="PageTwo" />
         </Scene>
       </Router>
